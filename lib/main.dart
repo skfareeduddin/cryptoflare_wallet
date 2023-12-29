3deb9d81729c8f91e2465a231d38ef9f4205704c
Syed Khaja Fareeduddin <skfareeduddin5@gmail.com>
2023-12-29 23:05:02 +0530

    Second commit.

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class CryptoFlare extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
           child: Center(
