9a6ff4f9308db05ac450a4e0173a78e48126ddd3
Syed Khaja Fareeduddin <skfareeduddin5@gmail.com>
2023-12-30 17:14:12 +0530

    Completed Verify Mnemonic screen functionality.

--- lib/pages/landing_screen.dart
+++ lib/pages/landing_screen.dart
@@ -1,6 +1,5 @@
 import 'package:cryptoflare_wallet/pages/generate_mnemonic_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class LandingScreen extends StatelessWidget {
   const LandingScreen({super.key});
