9181f8337724c228af8594c5c71e4d33eb228231
Syed Khaja Fareeduddin <skfareeduddin5@gmail.com>
2024-01-06 14:10:28 +0530

    Latest commit.

--- lib/pages/wallet_screen.dart
+++ lib/pages/wallet_screen.dart
@@ -218,7 +218,7 @@ class _WalletPageState extends State<WalletPage> {
                                             fontSize: 15.0,
                                             fontWeight: FontWeight.bold,
                                           ),
-                                        )
+                                        ),
                                       ],
                                     ),
                                   ),
